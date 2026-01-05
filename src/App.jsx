8fedb4faf4570e7d2dbe99abdfaa7f8f557a12cb
Shivam30Mishra <theshivammishra10@gmail.com>
2026-01-05 21:56:30 +0530

    add routing structure with base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+
+// pages
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Users from "./pages/Users/Users";
+
 function App() {
-  return <div>Admin Dashboard</div>
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
